90452e2afd038ece0beef271d41e6a1511313039
yara2512 <yaraisidro864@gmail.com>
2023-01-16 22:20:43 -0600

    Respaldo practica 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit(): void {
+    console.log("Hola mundo en IONIC");
+
+    let numero=20;
+    debugger;
+    numero=10;
+    numero=15;
+    numero=100;
+
+    console.log("La variable esta tomando este valor:"+numero);
+
+  }
 
 }
